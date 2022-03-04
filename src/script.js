76ff05efa149698fff0a4815a9506ee6f773b4ae
cmr211 <bartosz.surma211@gmail.com>
2022-03-04 10:08:03 +0100

    merge

--- src/script.js
+++ src/script.js
@@ -3,31 +3,45 @@ createSiteContent(data)
 function createSiteContent(links) {
     createCategories(links)
     createElements(links)
+    // createNavbar(links)
 }
 
 function createCategories(links) {
     const typesArray = Array.from(new Set(links.map((i) => i.type)))
     const container = document.getElementById('container')
-    typesArray.forEach((type) => {
+    typesArray.forEach((type, index) => {
         const subcontainer = document.createElement('div')
-        subcontainer.classList.add('grid', 'mt-5')
+        subcontainer.classList.add('grid')
         subcontainer.id = type
-        subcontainer.innerHTML = `<h2 class="grid-title">${type}</h2>`
+        subcontainer.innerHTML = `<h2 id="subtitle${index+1}" class="subtitle">${type}</h2>`
         container.appendChild(subcontainer)
     })
 }
 
+function createNavbar(links) {
+    const typesArray = Array.from(new Set(links.map((i) => i.type)))
+    const container = document.getElementById('nav')
+    typesArray.forEach((type, index) => {
+        const link = document.createElement('a')
+        link.classList.add('nav__item')
+        link.innerHTML = `${type}`
+        container.appendChild(link)
+    })
+}
+
 function createElements(links) {
-    links.forEach((i) => {
-        const card = document.createElement('div')
-        const container = document.getElementById(i.type)
-        card.classList.add('card')
-        card.innerHTML = `
+    links
+        .sort((a, b) => (a.title < b.title) ? -1 : 1)
+        .forEach((i) => {
+            const card = document.createElement('div')
+            const container = document.getElementById(i.type)
+            card.classList.add('card')
+            card.innerHTML = `
                 <a href=${i.url} class="link-reset">
                     <img class="card__img img-fluid" src='./assets/screenshots/${i.img}' alt="image of a ${i.title} website" />
-                    <h3 class="card__title my-4 mx-4">${i.title}</h1>
-                    <p class="card__desc m-4 small">#${i.id} ${i.desc}</p>
+                    <h3 class="card__title">${i.title}</h1>
+                    <p class="card__description"><span class="bold">#${i.id}</span> ${i.desc}</p>
                 </a>`
-        container.appendChild(card)
-    })
+            container.appendChild(card)
+        })
 }
